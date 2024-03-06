9aa7cde5c29908c7b226c410a60a79ddb4755793
s1ggelin <80677519+s1ggelin@users.noreply.github.com>
2024-03-06 14:09:57 +0100

    restructuring of files

--- vue-weather-app/src/components/TheWelcome.vue
+++ vue-weather-app/src/components/TheWelcome.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
+import DocumentationIcon from './icons/Examples/IconDocumentation.vue'
+import ToolingIcon from './icons/Examples/IconTooling.vue'
+import EcosystemIcon from './icons/Examples/IconEcosystem.vue'
+import CommunityIcon from './icons/Examples/IconCommunity.vue'
+import SupportIcon from './icons/Examples/IconSupport.vue'
 </script>
 
 <template>
